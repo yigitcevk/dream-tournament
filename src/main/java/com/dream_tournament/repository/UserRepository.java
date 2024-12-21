3ada65ad19fce864c4c8baa1140e52854fb5e75c
unknown <yigit.cevikk@gmail.com>
2024-12-22 01:39:08 +0300

    redundant import removed

--- src/main/java/com/dream_tournament/repository/UserRepository.java
+++ src/main/java/com/dream_tournament/repository/UserRepository.java
@@ -3,7 +3,5 @@ package com.dream_tournament.repository;
 import com.dream_tournament.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 }
