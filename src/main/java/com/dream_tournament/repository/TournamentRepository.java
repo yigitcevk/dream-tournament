78b3c4e0bd25833bf56616affbcf9dff660f9938
unknown <yigit.cevikk@gmail.com>
2024-12-22 01:43:31 +0300

    Tournament Repository created

--- src/main/java/com/dream_tournament/repository/TournamentRepository.java
+++ src/main/java/com/dream_tournament/repository/TournamentRepository.java
@@ -1,4 +1,9 @@
 package com.dream_tournament.repository;
 
-public interface TournamentRepository {
+import com.dream_tournament.model.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
 }
