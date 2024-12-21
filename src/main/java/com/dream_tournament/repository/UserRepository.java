0ffea80c12465c4bba86389f8da3fde004ef805e
unknown <yigit.cevikk@gmail.com>
2024-12-22 02:04:28 +0300

    findByUsername bug fix

--- src/main/java/com/dream_tournament/repository/UserRepository.java
+++ src/main/java/com/dream_tournament/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.dream_tournament.repository;
 import com.dream_tournament.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<Object> findByUsername(String username);
 }
