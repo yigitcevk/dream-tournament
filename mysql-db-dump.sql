dd81bb904c75406ef67c806c4d28ae38c3de9997
unknown <yigit.cevikk@gmail.com>
2024-12-24 07:28:01 +0300

    API & db-dump finished

--- mysql-db-dump.sql
+++ mysql-db-dump.sql
@@ -11,14 +11,15 @@ CREATE TABLE IF NOT EXISTS users (
     country VARCHAR(255) NOT NULL,
     level INT NOT NULL DEFAULT 1,
     coins INT NOT NULL DEFAULT 5000,
-    rewards_claimed BOOLEAN NOT NULL DEFAULT FALSE
+    active_tournament BOOLEAN NOT NULL DEFAULT FALSE
     );
 
 CREATE TABLE IF NOT EXISTS tournaments (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     tournament_name VARCHAR(255) NOT NULL,
     start_date DATE NOT NULL,
-    is_active BOOLEAN NOT NULL
+    is_active BOOLEAN NOT NULL,
+    latest BOOLEAN NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS tournament_group (
@@ -37,35 +38,34 @@ CREATE TABLE IF NOT EXISTS group_participant (
     );
 
 -- Insert example tournaments
-INSERT INTO tournaments (tournament_name, start_date, is_active)
+INSERT INTO tournaments (tournament_name, start_date, is_active, latest)
 VALUES
-    -- Active Tournament, Tournament of the day
-    ('Tournament-2024-12-22', '2024-12-22', TRUE),
     -- Old Tournaments
-    ('Tournament-2024-12-01', '2024-12-01', FALSE),
-    ('Tournament-2024-12-15', '2024-12-15', FALSE);
+    ('Tournament-2024-12-22', '2024-12-23', FALSE, TRUE),
+    ('Tournament-2024-12-01', '2024-12-01', FALSE, FALSE),
+    ('Tournament-2024-12-15', '2024-12-15', FALSE, FALSE);
 
 -- Insert example users
-INSERT INTO users (username, country, level, coins, rewards_claimed)
+INSERT INTO users (username, country, level, coins, active_tournament)
 VALUES
     -- Eligible players for the active tournament
-    ('Player1', 'Germany', 21, 6000, TRUE),
-    ('Player2', 'Turkey', 25, 7000, TRUE),
-    ('Player3', 'United States', 30, 8000, TRUE),
-    ('Player4', 'France', 35, 9000, TRUE),
-    ('Player5', 'United Kingdom', 26, 11000, TRUE),
-    ('Player6', 'Turkey', 40, 24000, TRUE),
-    ('Player7', 'France', 27, 13000, TRUE),
-    ('Player8', 'United States', 38, 17000, TRUE),
+    ('Player1', 'Germany', 21, 6000, FALSE),
+    ('Player2', 'Turkey', 25, 7000, FALSE),
+    ('Player3', 'United States', 30, 8000, FALSE),
+    ('Player4', 'France', 35, 9000, FALSE),
+    ('Player5', 'United Kingdom', 26, 11000, FALSE),
+    ('Player6', 'Turkey', 40, 24000, FALSE),
+    ('Player7', 'France', 27, 13000, FALSE),
     -- Ineligible players (low level or insufficient coins)
-    ('Player9', 'Germany', 19, 9000, TRUE),
-    ('Player10', 'Turkey', 22, 4000, TRUE),
-    ('Player11', 'France', 2, 6000, TRUE),
-    -- Players with unclaimed rewards
+    ('Player8', 'United States', 8, 17000, FALSE),
+    ('Player9', 'Germany', 19, 9000, FALSE),
+    ('Player10', 'Turkey', 22, 4000, FALSE),
+    ('Player11', 'France', 2, 6000, FALSE),
+    -- Also eligible players for the active tournament
     ('Player12', 'United States', 30, 8000, FALSE),
     ('Player13', 'France', 35, 9000, FALSE),
-    ('Player14', 'France', 24, 7000, FALSE),
-    ('Player15', 'France', 41, 14000, FALSE),
+    ('Player14', 'Germany', 24, 7000, FALSE),
+    ('Player15', 'Turkey', 41, 14000, FALSE),
     ('Player16', 'United Kingdom', 33, 16000, FALSE);
 
 
